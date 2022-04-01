30436eac6aaae7bc108ffa6be3b06cb1417834d8
Shashank Pali <shashank.pali@aventri.com>
2022-04-01 19:34:06 +0530

    handle multiple lines

--- Sources/PickerController/View/PickerCell/PickerCell.swift
+++ Sources/PickerController/View/PickerCell/PickerCell.swift
@@ -12,6 +12,8 @@ final class PickerCell: UITableViewCell {
     func setup(using model: PickerModel) {
         textLabel?.text = model.title
         textLabel?.font = UIFont.systemFont(ofSize: 20)
+        textLabel?.numberOfLines = 0
+        textLabel?.lineBreakMode = .byWordWrapping
         textLabel?.textColor = .darkGray
         accessoryType = model.isSelected ? .checkmark : .none
         //
